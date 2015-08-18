bf565cd898b6d19f627f99e95e0e9193d187c83c
Jimmy van den Berg <vandenberg.jimmy@gmail.com>
2015-08-18 13:15:14 +0200

    added the library files

---
+++ Vector3.cpp
@@ -0,0 +1,58 @@
+
+#include "Vector3.h"
+#include <math.h>
+
+Vector3::Vector3(float ix, float iy, float iz): x(ix), y(iy), z(iz){}
+Vector3::Vector3(float ia): x(ia), y(ia), z(ia){}
+Vector3::Vector3(): x(0), y(0), z(0){}
+
+const Vector3 Vector3::operator+(const Vector3& right) const{
+	Vector3 temp(*this);
+	temp += right;
+	return temp;
+}
+const Vector3 Vector3::operator-(const Vector3& right) const{
+	Vector3 temp(*this);
+	temp -= right;
+	return temp;
+}
+const Vector3 Vector3::operator*(const Vector3& right) const{
+	Vector3 temp(*this);
+	temp *= right;
+	return temp;
+}
+const Vector3 Vector3::operator/(const Vector3& right) const{
+	Vector3 temp(*this);
+	temp /= right;
+	return temp;
+}
+Vector3& Vector3::operator+=(const Vector3& right){
+	x += right.x;
+	y += right.y;
+	z += right.z;
+	return *this;
+}
+Vector3& Vector3::operator-=(const Vector3& right){
+	x -= right.x;
+	y -= right.y;
+	z -= right.z;
+	return *this;
+}
+Vector3& Vector3::operator*=(const Vector3& right){
+	x *= right.x;
+	y *= right.y;
+	z *= right.z;
+	return *this;
+}
+Vector3& Vector3::operator/=(const Vector3& right){
+	x /= right.x;
+	y /= right.y;
+	z /= right.z;
+	return *this;
+}
+void Vector3::normilize(){
+	float lenght = sqrt(pow(x,2) + pow(y,2) + pow(z,2));
+	x /= lenght;
+	y /= lenght;
+	z /= lenght;
+}
---
+++ Vector3.hpp
@@ -0,0 +1,25 @@
+
+#ifndef __Vector3_h__
+#define __Vector3_h__
+
+class Vector3{
+public:
+	Vector3(float ix, float iy, float iz);
+	Vector3(float ia);
+	Vector3();
+	float x,y,z;
+	const Vector3 operator+(const Vector3& right) const;
+	const Vector3 operator-(const Vector3& right) const;
+	const Vector3 operator*(const Vector3& right) const;
+	const Vector3 operator/(const Vector3& right) const;
+	Vector3& operator+=(const Vector3& right);
+	Vector3& operator-=(const Vector3& right);
+	Vector3& operator*=(const Vector3& right);
+	Vector3& operator/=(const Vector3& right);
+
+	void normilize();
+private:
+
+};
+
+#endif
